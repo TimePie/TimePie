a21db78aab039509996a0e4e3efeee9be541b46a
Stormmax <luyirenmax@gmail.com>
2014-04-16 09:11:14 +0800

    TimingItem.h TimingItem.m

---
+++ TimePie/TimePie/TimingItem.h
@@ -0,0 +1,13 @@
+//
+//  TimingItem.h
+//  TimePie
+//
+//  Created by Storm Max on 4/16/14.
+//  Copyright (c) 2014 TimePieOrg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimingItem : NSObject
+
+@end
---
+++ TimePie/TimePie/TimingItem.m
@@ -0,0 +1,13 @@
+//
+//  TimingItem.m
+//  TimePie
+//
+//  Created by Storm Max on 4/16/14.
+//  Copyright (c) 2014 TimePieOrg. All rights reserved.
+//
+
+#import "TimingItem.h"
+
+@implementation TimingItem
+
+@end
