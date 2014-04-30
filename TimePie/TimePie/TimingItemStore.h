150cd19fd880adfbed48942adc2dfa1322d164cf
Stormmax <luyirenmax@gmail.com>
2014-04-30 20:00:44 +0800

    [MainScreen]Pie chart && tableview [Data Model]TimingItem,  TimingItemStore(singleton)

---
+++ TimePie/TimePie/TimingItemStore.h
@@ -0,0 +1,13 @@
+//
+//  TimingItemStore.h
+//  TimePie
+//
+//  Created by Max Lu on 4/30/14.
+//  Copyright (c) 2014 TimePieOrg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimingItemStore : NSObject
+
+@end
---
+++ TimePie/TimePie/TimingItemStore.m
@@ -0,0 +1,13 @@
+//
+//  TimingItemStore.m
+//  TimePie
+//
+//  Created by Max Lu on 4/30/14.
+//  Copyright (c) 2014 TimePieOrg. All rights reserved.
+//
+
+#import "TimingItemStore.h"
+
+@implementation TimingItemStore
+
+@end
