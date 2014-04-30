295691b58def671cd2d69b6e13a9bd53cf04c866
Stormmax <luyirenmax@gmail.com>
2014-04-30 17:53:00 +0800

    Customized TableVIewCell

---
+++ TimePie/TimePie/TCell.h
@@ -0,0 +1,13 @@
+//
+//  TCell.h
+//  TimePie
+//
+//  Created by Max Lu on 4/30/14.
+//  Copyright (c) 2014 TimePieOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCell : UITableViewCell
+
+@end
--- TimePie/TimePie/TCell.m
+++ TimePie/TimePie/TCell.m
@@ -1,20 +1,14 @@
 //
-//  TimePieTableViewCell.m
+//  TCell.m
 //  TimePie
 //
-//  Created by Max Lu on 4/27/14.
+//  Created by Max Lu on 4/30/14.
 //  Copyright (c) 2014 TimePieOrg. All rights reserved.
 //
 
-#import "TimePieTableViewCell.h"
+#import "TCell.h"
 
-@implementation TimePieTableViewCell
-
-@synthesize itemName;
-@synthesize itemColor;
-@synthesize itemNotice;
-@synthesize itemTime;
-
+@implementation TCell
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
