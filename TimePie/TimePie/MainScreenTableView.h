d1a40bc4397ed154135156aec657e8c567437892
Stormmax <luyirenmax@gmail.com>
2014-05-01 10:45:21 +0800

    [MainScreen] Timer

---
+++ TimePie/TimePie/MainScreenTableView.h
@@ -0,0 +1,13 @@
+//
+//  MainScreenTableView.h
+//  TimePie
+//
+//  Created by Max Lu on 5/1/14.
+//  Copyright (c) 2014 TimePieOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainScreenTableView : UITableView
+
+@end
---
+++ TimePie/TimePie/MainScreenTableView.m
@@ -0,0 +1,31 @@
+//
+//  MainScreenTableView.m
+//  TimePie
+//
+//  Created by Max Lu on 5/1/14.
+//  Copyright (c) 2014 TimePieOrg. All rights reserved.
+//
+
+#import "MainScreenTableView.h"
+
+@implementation MainScreenTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
