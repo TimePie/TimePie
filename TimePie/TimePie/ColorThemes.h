f19fa462b950a6ca9a3cfa13610e3e10e24f25c8
Stormmax <luyirenmax@gmail.com>
2014-05-02 14:34:19 +0800

    [CreateItem]add item to TimingItemStore && CoreData [ColorTheme]Using singleton ColorTheme to manage colors and themes [CoreData] Update TimingItemEntity Attributes

---
+++ TimePie/TimePie/ColorThemes.h
@@ -0,0 +1,13 @@
+//
+//  ColorThemes.h
+//  TimePie
+//
+//  Created by Max Lu on 5/2/14.
+//  Copyright (c) 2014 TimePieOrg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ColorThemes : NSObject
+
+@end
---
+++ TimePie/TimePie/ColorThemes.m
@@ -0,0 +1,13 @@
+//
+//  ColorThemes.m
+//  TimePie
+//
+//  Created by Max Lu on 5/2/14.
+//  Copyright (c) 2014 TimePieOrg. All rights reserved.
+//
+
+#import "ColorThemes.h"
+
+@implementation ColorThemes
+
+@end
