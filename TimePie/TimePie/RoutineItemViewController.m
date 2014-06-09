e36505e5039ba9954639fe4210a7e404ac5e10d0
BillHHHuang <BillHuangg@gmail.com>
2014-06-09 20:05:12 +0800

    [例行事件表] 添加，获取，删除接口正常。但删除完，退回到个人中心时 崩溃。

--- TimePie/TimePie/RoutineItemViewController.m
+++ TimePie/TimePie/RoutineItemViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "RoutineItemViewController.h"
+#import "TimingItem1.h"
+#import "TimingItemEntity.h"
+#import "TimingItemStore.h"
+#import "Daily.h"
+#import "BasicUIColor+UIPosition.h"
 
 @interface RoutineItemViewController ()
 
@@ -28,7 +33,106 @@
     [super viewDidLoad];
     self.title = @"我的例行事件表";
     self.view.backgroundColor = [UIColor whiteColor];
+    [self initRoutineData];
+    [self initTableView];
 }
+
+- (void) initRoutineData
+{
+    self.routineDataArray = [[TimingItemStore timingItemStore] getAllDaily];
+}
+
+- (void) initTableView
+{
+    self.routineTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    self.routineTableView.dataSource = self;
+    self.routineTableView.delegate = self;
+    //self.routineTableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 10)];
+    [self.routineTableView setSeparatorInset:UIEdgeInsetsZero];
+    [self.view addSubview: self.routineTableView];
+    
+    [self setExtraCellLineHidden: self.routineTableView];
+}
+//delete useless lines
+-(void)setExtraCellLineHidden: (UITableView *)tableView
+{
+    UIView *view = [UIView new];
+    view.backgroundColor = [UIColor clearColor];
+    [tableView setTableFooterView:view];
+}
+
+
+
+#pragma mark - tableView datasource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.routineDataArray.count;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *cellIdentifier = @"RoutineTableViewCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if(cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifier];
+    }
+  
+    cell.textLabel.textColor = [UIColor colorWithRed:0.55 green:0.55 blue:0.55 alpha:1.f];
+    cell.textLabel.font = [UIFont fontWithName:@"Arial" size:16.f];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    int index = indexPath.row;
+    Daily *tempDaily = [self.routineDataArray objectAtIndex:index];
+    
+    cell.textLabel.text = tempDaily.item_name;
+    
+    return cell;
+}
+
+#pragma mark - UITableView Delegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 48;
+}
+
+
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return UITableViewCellEditingStyleDelete;
+}
+
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        //删除例行事件
+        int index = indexPath.row;
+        Daily *tempDaily = [self.routineDataArray objectAtIndex:index];
+
+        [[TimingItemStore timingItemStore] removeDaily:tempDaily.item_name];
+        [self initRoutineData];
+        [self.routineTableView reloadData];
+    }
+}
+
+
+//自定义 delete
+- (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return @"删除";
+}
+
 
 - (void)didReceiveMemoryWarning
 {
